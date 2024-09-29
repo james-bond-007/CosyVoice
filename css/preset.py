52a16e4547e721e15ad48297f0a6f7fdc93ee596
james-bond-007 <46163922+james-bond-007@users.noreply.github.com>
2024-09-29 15:35:58 +0800

    Update preset.py

--- css/preset.py
+++ css/preset.py
@@ -27,8 +27,8 @@ def preset():
         for output in model.inference_sft(_synthetic_input_textbox, _sound_radio):
             audio_tensors.append(output['tts_speech'])
             yield (target_sr, output['tts_speech'].numpy().flatten())
-        print("合成音频数组shape：{}".format(audio_tensors.shape))
-        print("合成音频：{} 秒".format(audio_tensors))
+        # print("合成音频数组shape：{}".format(audio_tensors.shape))
+        print("合成音频：{} ".format(audio_tensors))
         # 将所有音频片段连接成一个单独的Tensor
         full_audio = torch.concat(audio_tensors, dim=1)
         print("合成音频shape：{} ".format(full_audio.shape))
