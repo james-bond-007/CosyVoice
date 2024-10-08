677a33a81204a0abf788e5754980a8c47f50d4e4
james-bond-007 <46163922+james-bond-007@users.noreply.github.com>
2024-10-08 09:41:13 +0800

    Update advanced.py

--- css/advanced.py
+++ css/advanced.py
@@ -20,7 +20,7 @@ def advanced():
             gr.Warning('合成文本为空，您是否忘记输入合成文本？')
             return (target_sr, default_data)
         set_all_random_seed(_seed)
-        model = CosyVoice('pretrained_models/CosyVoice-300M-Instruct')
+        model = cosyvoice_instruct
         # output = model.inference_instruct(_synthetic_input_textbox, _sound_radio, _speech_status_textbox)
         audio_tensors = []
         for output in model.inference_instruct(_synthetic_input_textbox, _sound_radio, _speech_status_textbox, stream=True, speed=1.0):
